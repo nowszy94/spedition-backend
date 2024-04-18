cfeb3d97e95748ac4139e9b4b1133101b3620359
Szymon Nowak <nowszy94@gmail.com>
2024-04-19 01:11:19 +0200

    default momentjs locale

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+import * as moment from 'moment';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { UnauthenticatedExceptionFilter } from './auth/exceptions/unauthenticated.exception';
@@ -7,5 +8,7 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalFilters(new UnauthenticatedExceptionFilter());
   await app.listen(80);
+
+  moment.locale('pl-PL');
 }
 bootstrap();
